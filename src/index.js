d487dc79c9d0dd141520970f2f0b3d69e8496b4e
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-03-13 13:16:10 -0500

    demo1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ require('offline-plugin/runtime').install();
 import './fonts/libre-baskerville-v5-latin-regular.woff';
 import './fonts/libre-baskerville-v5-latin-regular.woff2';
 
-import './index.html';
+import '../index.html';
 import './index.scss';
 import './scripts/script.js';
