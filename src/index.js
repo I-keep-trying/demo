e0008f15d5d7219d0347adfd3b5b533b079a2f71
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2019-03-28 13:32:21 -0500

    webpack build

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ require('offline-plugin/runtime').install();
 
 import '../index.html';
 import './index.scss';
-import './scripts/script.js';
+//import './scripts/script.js';
 
+
+import '../dist/webpack-bundle.js'; 
